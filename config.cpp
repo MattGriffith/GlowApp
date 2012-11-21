52762f7d6d28caab9b5db99ca2838ef309cf8bd6
Matt <example@example.com>
2012-11-21 14:04:26 -0600

    Distributing this program under the GNU General Public License. Fixed the readme file, added the copying file, added the GPL notice and copyright at the top of each source code file. Cleaned up the repository for the initial upload to GitHub. Yes, the screenshot folder is getting a little out of hand. Oh well.

--- config.cpp
+++ config.cpp
@@ -1,6 +1,25 @@
+/*
 
-// Easy way to read .ini configuration files
-// Written by Matthew Griffith
+    Easy way to read .ini configuration files
+
+    Copyright 2007, 2012 Matthew Griffith
+
+    This file is part of TroidGlow.
+
+    TroidGlow is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TroidGlow is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TroidGlow.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 #include "config.h"
 #include <fstream>
